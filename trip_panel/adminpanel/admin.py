b73edaf232826c32ebfc7a6aefe76703125b3cd0
mohammadreza <mohammadrezakhalili08@gmail.com>
2022-11-29 17:04:59 +0330

    models were added

--- trip_panel/adminpanel/admin.py
+++ trip_panel/adminpanel/admin.py
@@ -1,2 +1,6 @@
 from django.contrib import admin
+from adminpanel.models import hotell
 
+
+
+admin.site.register(hotell)
--- trip_panel/adminpanel/models.py
+++ trip_panel/adminpanel/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class hotell(models.Model):
+    hotelname     = models.CharField(max_length=50)
+    hotellpics    = models.ImageField( null= True, blank=True , upload_to='images/' )
+    timestamp     = models.DateTimeField(auto_now_add=True)
+    date          = models.DateTimeField(auto_now_add=True)
+    hotellinfo    = models.TextField()
+
+    
+
+
+    def __str__(self) -> str:
+        return self.hotelname
