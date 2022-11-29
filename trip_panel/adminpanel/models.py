a02bd0924e0b840392495e8f34ef90a4490d6e03
mohammadreza <mohammadrezakhalili08@gmail.com>
2022-11-29 18:27:59 +0330

    models updated

--- trip_panel/adminpanel/models.py
+++ trip_panel/adminpanel/models.py
@@ -3,12 +3,11 @@ from django.db import models
 # Create your models here.
 
 class hotell(models.Model):
-    hotelname     = models.CharField(max_length=50)
-    hottladdress  = models.CharField(max_length=250)
-    hotellpics    = models.ImageField( null= True, blank=True , upload_to='images/' )
-    timestamp     = models.DateTimeField(auto_now_add=True)
-    date          = models.DateTimeField(auto_now_add=True)
-    hotel_info    = models.TextField()
+    hotel_name     = models.CharField(max_length=50)
+    hottl_address  = models.CharField(max_length=250)
+    hotel_pics     = models.ImageField( null= True, blank=True , upload_to='images/' )
+    timestamp      = models.DateTimeField(auto_now_add=True)
+    hotel_info     = models.TextField()
 
     
 
