33dbdada992fbbd0690a63c7bc2da09437141c21
amirjahandar <amirjahanwz@gmail.com>
2022-12-01 20:15:28 +0330

    trip-panel project completed it  works correctly

--- trip_panel/adminpanel/admin.py
+++ trip_panel/adminpanel/admin.py
@@ -5,8 +5,9 @@ from adminpanel.models import Hotel
 
 @admin.register(Hotel)
 class HotelAdmin(admin.ModelAdmin):
-    list_display = ['id', 'hotel_name',  'score', 'insert_time','hotel_pictures', ]
-    fields = ['hotel_name', 'hotel_pictures', ('hotel_info', 'hotel_address'), 'score',  ]
+    list_display = ['id', 'hotel_name',  'score', 'insert_time','hotel_pictures', 'image_tag']
+    fields = ['hotel_name', 'hotel_pictures', ('hotel_info', 'hotel_address'), 'score', 'image_tag']
+    readonly_fields = ['image_tag']
     sortable_by = ['hotel_name', 'score', 'insert_time']
     list_editable = ['score']
     list_filter = ['hotel_address']
@@ -15,7 +16,7 @@ class HotelAdmin(admin.ModelAdmin):
 
     def image_tag(self, obj):
 
-        return format_html('<img src="{}" width="200" height="200"/>'.format(obj.hotel_pictures.url))
+        return format_html('<img src="{url}" width="300" height="200"/>'.format(url = obj.hotel_pictures.url))
 
     image_tag.short_description = 'Image'
 
