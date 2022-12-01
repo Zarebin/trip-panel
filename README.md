94e3b28956c0c2fd557dcd01d3ce0c408f3fb2a3
amirjahandar <amirjahanwz@gmail.com>
2022-12-01 20:15:28 +0330

    trip-panel project completed it  works correctly

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ that use for insert main content of site
 ### Note
 
 first make virtual enviroment then install requirements.txt
+in this project use grappelli theme for admin panel 
+configuration set in settings.py and files are exist in static directory
--- trip_panel/adminpanel/models.py
+++ trip_panel/adminpanel/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.utils.html import escape
+
 
 
 
@@ -12,13 +12,8 @@ class Hotel(models.Model):
     hotel_address = models.CharField(max_length=550)
 
     
-    def image_tag(self):
-        return u'<img src="%s" />' % escape(self.hotel_pictures.url)
-
-
-    image_tag.short_description = 'hotel_pictures'
-    image_tag.allow_tags = True
-
 
     def __str__(self) -> str:
         return self.hotel_name
+
+
